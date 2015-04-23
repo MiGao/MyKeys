0de141bdebef03de393699bee598e1c1677d7dd8
MiGao <xage283@gmail.com>
2015-04-22 19:40:34 -0500

    Made some name changes to Activity classes. Added a user_name column to the Account table.

--- app/src/main/java/com/migao/mykeys/database/account/AccountContract.java
+++ app/src/main/java/com/migao/mykeys/database/account/AccountContract.java
@@ -8,13 +8,18 @@ import android.provider.BaseColumns;
 public final class AccountContract {
     public static final String SQL_CREATE_TABLE =
             "CREATE TABLE " + AccountEntry.TABLE_NAME + " (" +
-                    AccountEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    AccountEntry.COLUMN_NAME_ACCOUNT_NAME + " TEXT NOT NULL);";
+                    AccountEntry._ID + " INTEGER AUTOINCREMENT, " +
+                    AccountEntry.COLUMN_NAME_ACCOUNT_NAME + " TEXT NOT NULL, " +
+                    AccountEntry.COLUMN_NAME_USER_NAME + " TEXT NOT NULL, " +
+                    " PRIMARY KEY(" + AccountEntry._ID + ", " +
+                    AccountEntry.COLUMN_NAME_ACCOUNT_NAME + ", " +
+                    AccountEntry.COLUMN_NAME_USER_NAME + "));";
 
     public AccountContract() {}
 
     public static abstract class AccountEntry implements BaseColumns {
         public static final String TABLE_NAME = "Account";
         public static final String COLUMN_NAME_ACCOUNT_NAME = "account_name";
+        public static final String COLUMN_NAME_USER_NAME = "user_name";
     }
 }
